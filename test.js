25817fd647340e7345c445c20d8396173b8ad6a5
Filipe Romano Rodrigues <filiperr@br.ibm.com>
2019-02-14 14:02:14 -0200

    add weather test

---
+++ test.js
@@ -0,0 +1,16 @@
+var request = require('xhr-request')
+ 
+request('https://b9eae2fb-e9e9-475c-8561-0549f39e21a3:aE7Uql75X7@twcservice.mybluemix.net:443/api/weather/v1/geocode/-23.68/-46.87/observations.json', {
+  method: 'GET',
+  json: true,
+  qs: {
+    units: "m",
+    language: "pt-br"  
+  }
+}, function (err, data) {
+  if (err) {
+    console.error(err); // something went wrong
+    return;
+  }
+  console.log('Temperature: ', data.observation.temp)
+})
